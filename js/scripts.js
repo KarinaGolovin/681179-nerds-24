66573b70da198e95cf3b5d7ff0e707b5c1590eab
Karina Golovin <karina.golovin@gmail.com>
2019-02-13 22:04:32 +0200

    Carousel slider animation, corrected several layout mistakes

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 'use strict';
 
 (function() {
-  var KEY_CODES = {
+  const KEY_CODES = {
     enter: 13,
     esc: 27
   };
 
-  var modalOpenButton = document.querySelector('.address__link');
-  var modalWindow = document.querySelector('.modal');
-  var modalContent = document.querySelector('.modal__content');
-  var modalCloseButton = modalWindow.querySelector('.modal__close');
-  var modalSubmitButton = modalWindow.querySelector('.contact-form__btn');
+  const modalOpenButton = document.querySelector('.address__link');
+  const modalWindow = document.querySelector('.modal');
+  const modalContent = document.querySelector('.modal__content');
+  const modalCloseButton = modalWindow.querySelector('.modal__close');
+  const modalSubmitButton = modalWindow.querySelector('.contact-form__btn');
+  const modalOverlay = document.querySelector('.overlay');
+  const contactForm = modalWindow.querySelector('.contact-form');
 
   function openModal(event) {
-    console.log('Its works')
     event.preventDefault();
     modalWindow.classList.remove('modal--disable');
     modalOpenButton.blur();
@@ -29,12 +30,27 @@
 
   function closeModal() {
     modalWindow.classList.add('modal--disable');
+    contactForm.reset();
     focusLock.off(modalContent);
   };
 
   function closeModalOnEnter(event) {
     if (event.keyCode === KEY_CODES.enter) {
       event.preventDefault();
+      closeModal();
+    }
+  };
+
+  function closeModalOnEsc(event) {
+    if (event.keyCode === KEY_CODES.esc) {
+      event.preventDefault();
+      closeModal();
+    }
+  };
+
+  function closeModalOnOutClick(event) {
+    event.preventDefault();
+    if (event.target === modalOverlay) {
       closeModal();
     }
   };
@@ -58,17 +74,47 @@
   modalCloseButton.addEventListener('blur', function() {
     modalCloseButton.addEventListener('keydown', closeModalOnEnter);
   })
+  document.addEventListener('keydown', closeModalOnEsc);
+  modalWindow.addEventListener('click', closeModalOnOutClick);
 
-  document.addEventListener('keydown', function(event) {
-    if (event.keyCode === KEY_CODES.esc) {
-      event.preventDefault();
-      closeModal();
-    }
-  });
-
+  // Input validity check?
 
 
   // Submit modal
 
+  // Slider
+  document.querySelectorAll('.slider').forEach(function (slider) {
+    const sliderButtons = slider.querySelectorAll('.slider-switcher__button');
+
+    let activeSlide = slider.querySelector('.slider-element--active');
+    let currentSliderId = activeSlide.getAttribute('data-id');
+    let activeButton = slider.querySelector('.slider-switcher__button[data-id=' + currentSliderId + ']');
+
+    function handleSliderButtonClick(event) {
+      let clickedButton = event.currentTarget;
+      let nextSlideId = clickedButton.getAttribute('data-id');
+
+      if (nextSlideId === currentSliderId) {
+        return;
+      }
+
+      activeSlide.classList.remove('slider-element--active');
+      activeButton.classList.remove('slider-switcher__button--active');
+
+      let nextSlide = slider.querySelector('.slider__slide[data-id=' + nextSlideId + ']');
+      let nextButton = clickedButton;
+
+      nextSlide.classList.add('slider-element--active');
+      nextButton.classList.add('slider-switcher__button--active');
+
+      activeSlide = nextSlide;
+      activeButton = nextButton;
+      currentSliderId = nextSlideId;
+    };
+
+    sliderButtons.forEach(function (element) {
+      element.addEventListener('click', handleSliderButtonClick);
+    });
+  });
 })();
 
