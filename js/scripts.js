3f03ea219bf2e350b720d7624b9d7f3a50ef1ac7
Karina Golovin <karina.golovin@gmail.com>
2019-02-18 12:50:46 +0200

    Fixed width, modal, some input colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,97 @@
 'use strict';
 
 (function() {
-  const KEY_CODES = {
-    enter: 13,
-    esc: 27
-  };
-
   const modalOpenButton = document.querySelector('.address__link');
-  const modalWindow = document.querySelector('.modal');
-  const modalContent = document.querySelector('.modal__content');
-  const modalCloseButton = modalWindow.querySelector('.modal__close');
-  const modalSubmitButton = modalWindow.querySelector('.contact-form__btn');
-  const modalOverlay = document.querySelector('.overlay');
-  const contactForm = modalWindow.querySelector('.contact-form');
-
-  function openModal(event) {
+  modalOpenButton.addEventListener('click', function (event) {
     event.preventDefault();
-    modalWindow.classList.remove('modal--disable');
-    modalOpenButton.blur();
-    focusLock.on(modalContent);
-  }
-
-  function openModalOnEnter(event) {
-    if (event.keyCode === KEY_CODES.enter) {
-      event.preventDefault();
-      openModal();
-    }
-  };
-
-  function closeModal() {
-    modalWindow.classList.add('modal--disable');
-    contactForm.reset();
-    focusLock.off(modalContent);
-  };
-
-  function closeModalOnEnter(event) {
-    if (event.keyCode === KEY_CODES.enter) {
-      event.preventDefault();
-      closeModal();
-    }
-  };
-
-  function closeModalOnEsc(event) {
-    if (event.keyCode === KEY_CODES.esc) {
-      event.preventDefault();
-      closeModal();
-    }
-  };
-
-  function closeModalOnOutClick(event) {
-    event.preventDefault();
-    if (event.target === modalOverlay) {
-      closeModal();
-    }
-  };
-
-  //Open Modal
-  modalOpenButton.addEventListener('click', openModal);
-
-  modalOpenButton.addEventListener('focus', function() {
-    modalOpenButton.addEventListener('keydown', openModalOnEnter);
   });
 
-  modalOpenButton.addEventListener('blur', function() {
-    modalOpenButton.removeEventListener('keydown', openModalOnEnter);
-  })
-
-  //Close
-  modalCloseButton.addEventListener('click', closeModal);
-  modalCloseButton.addEventListener('focus', function() {
-    modalCloseButton.addEventListener('keydown', closeModalOnEnter);
+  /**
+   * Native dialog element not used due to the:
+   * https://github.com/edenspiekermann/a11y-dialog/issues/89
+   */
+  document.addEventListener('DOMContentLoaded', function () {
+    var dialogEl = document.getElementById('my-accessible-dialog');
+    var mainEl = document.querySelector('.content--shop');
+    var overlay = dialogEl.querySelector('.dialog-overlay');
+    var dialog = new window.A11yDialog(dialogEl, mainEl);
   });
-  modalCloseButton.addEventListener('blur', function() {
-    modalCloseButton.addEventListener('keydown', closeModalOnEnter);
-  })
-  document.addEventListener('keydown', closeModalOnEsc);
-  modalWindow.addEventListener('click', closeModalOnOutClick);
+
+//   const KEY_CODES = {
+//     enter: 13,
+//     esc: 27
+//   };
+
+//   const modalOpenButton = document.querySelector('.address__link');
+//   const modalWindow = document.querySelector('.modal');
+//   const modalContent = document.querySelector('.modal__content');
+//   const modalCloseButton = modalWindow.querySelector('.modal__close');
+//   const modalSubmitButton = modalWindow.querySelector('.contact-form__btn');
+//   const modalOverlay = document.querySelector('.overlay');
+//   const contactForm = modalWindow.querySelector('.contact-form');
+
+//   function openModal(event) {
+//     event.preventDefault();
+//     modalWindow.classList.remove('modal--disable');
+//     modalOpenButton.blur();
+//     focusLock.on(modalContent);
+//   }
+
+//   function openModalOnEnter(event) {
+//     if (event.keyCode === KEY_CODES.enter) {
+//       event.preventDefault();
+//       openModal();
+//     }
+//   };
+
+//   function closeModal() {
+//     modalWindow.classList.add('modal--disable');
+//     contactForm.reset();
+//     focusLock.off(modalContent);
+//   };
+
+//   function closeModalOnEnter(event) {
+//     if (event.keyCode === KEY_CODES.enter) {
+//       event.preventDefault();
+//       closeModal();
+//     }
+//   };
+
+//   function closeModalOnEsc(event) {
+//     if (event.keyCode === KEY_CODES.esc) {
+//       event.preventDefault();
+//       closeModal();
+//     }
+//   };
+
+//   function closeModalOnOutClick(event) {
+//     event.preventDefault();
+//     if (event.target === modalOverlay) {
+//       closeModal();
+//     }
+//   };
+
+//   //Open Modal
+//   modalOpenButton.addEventListener('click', openModal);
+
+//   modalOpenButton.addEventListener('focus', function() {
+//     modalOpenButton.addEventListener('keydown', openModalOnEnter);
+//   });
+
+//   modalOpenButton.addEventListener('blur', function() {
+//     modalOpenButton.removeEventListener('keydown', openModalOnEnter);
+//   })
+
+//   //Close
+//   modalCloseButton.addEventListener('click', closeModal);
+//   modalCloseButton.addEventListener('focus', function() {
+//     modalCloseButton.addEventListener('keydown', closeModalOnEnter);
+//   });
+//   modalCloseButton.addEventListener('blur', function() {
+//     modalCloseButton.addEventListener('keydown', closeModalOnEnter);
+//   })
+//   document.addEventListener('keydown', closeModalOnEsc);
+//   modalWindow.addEventListener('click', closeModalOnOutClick);
 
   // Input validity check?
 
