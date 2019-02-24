781504cf921a656f09724a52afe2c3b45b5264c0
Karina Golovin <karina.golovin@gmail.com>
2019-02-24 13:50:06 +0200

    test that pages rendered correctly

---
+++ __tests__/render.test.js
@@ -0,0 +1,38 @@
+// eslint is looking for `puppeteer` at root level package.json
+// eslint-disable-next-line import/no-unresolved
+const puppeteer = require('puppeteer');
+const pageWaitTimeout = 1200;
+
+jest.setTimeout(10000);
+
+describe('check that pages renders correctly', () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+  });
+
+  it('renders home page correctly', async () => {
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1600, height: 2500 });
+    await page.goto('http://127.0.0.1:5500/');
+    await page.waitFor(pageWaitTimeout);
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it('renders catalog page correctly', async () => {
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1600, height: 2500 });
+    await page.goto('http://127.0.0.1:5500/catalog');
+    await page.waitFor(pageWaitTimeout);
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
