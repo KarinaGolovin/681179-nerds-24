1aeb2ee458325516cf042ed336bf6b836897a8fc
Karina Golovin <karina.golovin@gmail.com>
2019-02-25 14:43:14 +0200

    modal window fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,102 +1,114 @@
 'use strict';
 
 (function() {
+  // const modalOpenButton = document.querySelector('.address__link');
+  // modalOpenButton.addEventListener('click', function (event) {
+  //   event.preventDefault();
+  // });
+
+  // /**
+  //  * Native dialog element not used due to the:
+  //  * https://github.com/edenspiekermann/a11y-dialog/issues/89
+  //  */
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   var dialogEl = document.getElementById('my-accessible-dialog');
+  //   var mainEl = document.querySelector('.content--shop');
+  //   var overlay = dialogEl.querySelector('.dialog-overlay');
+  //   // var dialog = new window.A11yDialog(dialogEl, mainEl);
+  // });
+
+  const KEY_CODES = {
+    enter: 13,
+    esc: 27
+  };
+
   const modalOpenButton = document.querySelector('.address__link');
-  modalOpenButton.addEventListener('click', function (event) {
+  const modalWindow = document.querySelector('.modal');
+  const modalContent = document.querySelector('.modal__content');
+  const modalCloseButton = modalWindow.querySelector('.modal__close');
+  const modalOverlay = document.querySelector('.overlay');
+  const contactForm = modalWindow.querySelector('.contact-form');
+  const firstModalInput = modalWindow.querySelector('.contact-form__input');
+
+  function openModal(event) {
     event.preventDefault();
+    modalWindow.classList.remove('modal--disable');
+    modalContent.classList.add('.modal__content--active');
+    firstModalInput.focus();
+    modalOpenButton.blur();
+    // focusLock.on(modalContent);
+  }
+
+  function openModalOnEnter(event) {
+    if (event.keyCode === KEY_CODES.enter) {
+      event.preventDefault();
+      openModal();
+    }
+  };
+
+  function closeModal() {
+    modalContent.classList.remove('modal__content--active');
+    modalContent.classList.remove('modal__content--error');
+    modalWindow.classList.add('modal--disable');
+    contactForm.reset();
+    // focusLock.off(modalContent);
+  };
+
+  function closeModalOnEnter(event) {
+    if (event.keyCode === KEY_CODES.enter) {
+      event.preventDefault();
+      closeModal();
+    }
+  };
+
+  function closeModalOnEsc(event) {
+    if (event.keyCode === KEY_CODES.esc) {
+      event.preventDefault();
+      closeModal();
+    }
+  };
+
+  function closeModalOnOutClick(event) {
+    event.preventDefault();
+    if (event.target === modalOverlay) {
+      closeModal();
+    }
+  };
+
+  //Open Modal
+  modalOpenButton.addEventListener('click', openModal);
+
+  modalOpenButton.addEventListener('focus', function() {
+    modalOpenButton.addEventListener('keydown', openModalOnEnter);
   });
 
-  /**
-   * Native dialog element not used due to the:
-   * https://github.com/edenspiekermann/a11y-dialog/issues/89
-   */
-  document.addEventListener('DOMContentLoaded', function () {
-    var dialogEl = document.getElementById('my-accessible-dialog');
-    var mainEl = document.querySelector('.content--shop');
-    var overlay = dialogEl.querySelector('.dialog-overlay');
-    var dialog = new window.A11yDialog(dialogEl, mainEl);
+  modalOpenButton.addEventListener('blur', function() {
+    modalOpenButton.removeEventListener('keydown', openModalOnEnter);
+  })
+
+  //Close
+  modalCloseButton.addEventListener('click', closeModal);
+  modalCloseButton.addEventListener('focus', function() {
+    modalCloseButton.addEventListener('keydown', closeModalOnEnter);
   });
+  modalCloseButton.addEventListener('blur', function() {
+    modalCloseButton.addEventListener('keydown', closeModalOnEnter);
+  })
+  document.addEventListener('keydown', closeModalOnEsc);
+  modalWindow.addEventListener('click', closeModalOnOutClick);
 
-//   const KEY_CODES = {
-//     enter: 13,
-//     esc: 27
-//   };
+  // Input validity check and Submit modal
+  const nameInput = modalWindow.querySelector('[name=name-input]');
+  const mailInput = modalWindow.querySelector('[name=email-input]');
 
-//   const modalOpenButton = document.querySelector('.address__link');
-//   const modalWindow = document.querySelector('.modal');
-//   const modalContent = document.querySelector('.modal__content');
-//   const modalCloseButton = modalWindow.querySelector('.modal__close');
-//   const modalSubmitButton = modalWindow.querySelector('.contact-form__btn');
-//   const modalOverlay = document.querySelector('.overlay');
-//   const contactForm = modalWindow.querySelector('.contact-form');
-
-//   function openModal(event) {
-//     event.preventDefault();
-//     modalWindow.classList.remove('modal--disable');
-//     modalOpenButton.blur();
-//     focusLock.on(modalContent);
-//   }
-
-//   function openModalOnEnter(event) {
-//     if (event.keyCode === KEY_CODES.enter) {
-//       event.preventDefault();
-//       openModal();
-//     }
-//   };
-
-//   function closeModal() {
-//     modalWindow.classList.add('modal--disable');
-//     contactForm.reset();
-//     focusLock.off(modalContent);
-//   };
-
-//   function closeModalOnEnter(event) {
-//     if (event.keyCode === KEY_CODES.enter) {
-//       event.preventDefault();
-//       closeModal();
-//     }
-//   };
-
-//   function closeModalOnEsc(event) {
-//     if (event.keyCode === KEY_CODES.esc) {
-//       event.preventDefault();
-//       closeModal();
-//     }
-//   };
-
-//   function closeModalOnOutClick(event) {
-//     event.preventDefault();
-//     if (event.target === modalOverlay) {
-//       closeModal();
-//     }
-//   };
-
-//   //Open Modal
-//   modalOpenButton.addEventListener('click', openModal);
-
-//   modalOpenButton.addEventListener('focus', function() {
-//     modalOpenButton.addEventListener('keydown', openModalOnEnter);
-//   });
-
-//   modalOpenButton.addEventListener('blur', function() {
-//     modalOpenButton.removeEventListener('keydown', openModalOnEnter);
-//   })
-
-//   //Close
-//   modalCloseButton.addEventListener('click', closeModal);
-//   modalCloseButton.addEventListener('focus', function() {
-//     modalCloseButton.addEventListener('keydown', closeModalOnEnter);
-//   });
-//   modalCloseButton.addEventListener('blur', function() {
-//     modalCloseButton.addEventListener('keydown', closeModalOnEnter);
-//   })
-//   document.addEventListener('keydown', closeModalOnEsc);
-//   modalWindow.addEventListener('click', closeModalOnOutClick);
-
-  // Input validity check?
-
-
-  // Submit modal
+  contactForm.addEventListener('submit', function(event) {
+    if (!nameInput || !mailInput) {
+      event, preventDefault();
+      modalContent.classList.remove('modal__content--error');
+      modalContent.classList.add('modal__content--error');
+      console.log("Нужно ввести имя и адрес");
+    }
+  })
 
   // Slider
   function toArray(nodes) {
@@ -104,11 +116,11 @@
   }
 
   toArray(document.querySelectorAll('.slider')).forEach(function (slider) {
-    const sliderButtons = toArray(slider.querySelectorAll('.slider-switcher__button'));
+    const sliderButtons = toArray(slider.querySelectorAll('.switch-panel__button'));
 
     let activeSlide = slider.querySelector('.slide--active');
     let currentSliderId = activeSlide.getAttribute('data-id');
-    let activeButton = slider.querySelector('.slider-switcher__button[data-id=' + currentSliderId + ']');
+    let activeButton = slider.querySelector('.switch-panel__button[data-id=' + currentSliderId + ']');
 
     function handleSliderButtonClick(event) {
       let clickedButton = event.currentTarget;
@@ -119,13 +131,13 @@
       }
 
       activeSlide.classList.remove('slide--active');
-      activeButton.classList.remove('slider-switcher__button--active');
+      activeButton.classList.remove('switch-panel__button--active');
 
       let nextSlide = slider.querySelector('.slider__element[data-id=' + nextSlideId + ']');
       let nextButton = clickedButton;
 
       nextSlide.classList.add('slide--active');
-      nextButton.classList.add('slider-switcher__button--active');
+      nextButton.classList.add('switch-panel__button--active');
 
       activeSlide = nextSlide;
       activeButton = nextButton;
